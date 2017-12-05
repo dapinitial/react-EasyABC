77f7458ea7ea5eb1c326a065687a870c1f4a346a
David Puerto <me@davidpuerto.com>
2017-12-04 20:52:27 -0800

    adding some functions and styling

--- src/EasyABC.js
+++ src/EasyABC.js
@@ -1,28 +1,93 @@
 import React, { Component } from "react";
+import alphabets from "./alphabets.json";
+import classnames from "classnames";
 
 class EasyABC extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      alphabets: alphabets,
+      currentPosition: 0,
+      currentTick: 0
+    };
+    this.next = this.next.bind(this);
+    this.prev = this.prev.bind(this);
+    this.playSound = this.playSound.bind(this);
+  }
+
+  prev() {
+    console.log("prev button clicked");
+    if (this.state.currentTick < 2) {
+      this.setState({ currentTick: this.state.currentTick - 1 });
+    } else {
+      this.setState({ currentPosition: this.state.currentPosition - 1 });
+    }
+  }
+
+  next() {
+    console.log("next button clicked");
+    if (this.state.currentTick < 2) {
+      this.setState({ currentTick: this.state.currentTick + 1 });
+    } else {
+      this.setState({ currentPosition: this.state.currentPosition + 1 });
+    }
+  }
+
+  playSound() {
+    console.log("playSound button clicked");
+  }
+
   render() {
+    //console.log(alphabets);
+    let showImage = this.state.currentTick !== 0 ? true : false;
+    let showWord = this.state.currentTick === 2 ? true : false;
+    console.log(this.state.currentTick, showImage);
     return (
       <div className="game">
         <div className="option">
           <div className="fields">
-            <div className="field-block">A</div>
+            <div className="field-block">
+              {this.state.alphabets[this.state.currentPosition].letter}
+            </div>
           </div>
           <div className="buttons">
-            <a href="#" className="button previous">
+            <button className="button previous" onClick={this.prev}>
               Previous
-            </a>
-            <a href="#" className="button sound">
+            </button>
+            <button className="button sound" onClick={this.playSound}>
               Play Sound Again
-            </a>
-            <a href="#" className="button next">
+            </button>
+            <button className="button next" onClick={this.next}>
               Next
-            </a>
+            </button>
           </div>
           <div className="fields">
             <div className="field-block">
-              <div className="left-field">B</div>
-              <div className="right-field">C</div>
+              <div className="left-field">
+                <div
+                  className={classnames("placeholder-span", {
+                    hide: showImage
+                  })}
+                >
+                  Click Next to view image
+                </div>
+                <img
+                  className={classnames("letter-image", { hide: !showImage })}
+                  src={this.state.alphabets[this.state.currentPosition].image}
+                  alt={this.state.alphabets[this.state.currentPosition].word}
+                />
+              </div>
+              <div className="right-field">
+                <div
+                  className={classnames("placeholder-span", { hide: showWord })}
+                >
+                  Spelling
+                </div>
+                <div className={classnames("word", { hide: !showImage })}>
+                  {this.state.alphabets[this.state.currentPosition].word}
+                </div>
+              </div>
             </div>
           </div>
         </div>
